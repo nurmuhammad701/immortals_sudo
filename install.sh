b009c524e987e26c7c11e4bb43040695713c5d63
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-09-08 20:56:08 +0500

    Dastlabki fayllar va o'rnatish skripti qo'shildi

--- install.sh
+++ install.sh
@@ -11,13 +11,34 @@ if ! command -v go &> /dev/null; then
     apt-get install -y golang
 fi
 
-# Loyihani yuklab olish va kompilyatsiya qilish
-echo "Manba kodini yuklab olish va kompilyatsiya qilish..."
-go install github.com/nurmuhammad701/immortals_sudo@latest
+# Loyihani yuklab olish
+echo "Manba kodini yuklab olish..."
+git clone https://github.com/nurmuhammad701/immortals_sudo.git
+cd immortals_sudo
 
-# Binarni /usr/local/bin ga ko'chirish
-echo "Binarni tizim katalogiga ko'chirish..."
-install -m 755 ~/go/bin/immortals_sudo /usr/local/bin/immortals
+# Currency va Todo dasturlarini alohida build qilish
+echo "Currency dasturini build qilish..."
+go build -o currency ./Currency
+echo "Todo dasturini build qilish..."
+go build -o todo ./todos
+
+# Binarlarni /usr/local/bin ga ko'chirish
+echo "Binarlarni tizim katalogiga ko'chirish..."
+install -m 755 currency /usr/local/bin/immortals-currency
+install -m 755 todo /usr/local/bin/immortals-todo
+
+# Asosiy immortals buyrug'ini yaratish
+echo '#!/bin/bash
+case $1 in
+  "currency") shift; immortals-currency "$@" ;;
+  "todo") shift; immortals-todo "$@" ;;
+  *) echo "Noto'g'ri buyruq. 'currency' yoki 'todo' dan foydalaning." ;;
+esac' > immortals
+install -m 755 immortals /usr/local/bin/immortals
 
 echo "Immortals muvaffaqiyatli o'rnatildi!"
 echo "Quyidagi buyruqlar bilan ishlatishingiz mumkin: 'immortals todo' va 'immortals currency'"
+
+# O'rnatish papkasini tozalash
+cd ..
+rm -rf immortals_sudo
